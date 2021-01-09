a119152e81e3d9b64a35448ac7c3b027a482c106
asahapde <60831459+asahapde@users.noreply.github.com>
2021-01-08 21:41:04 -0500

    Add form validation

--- CheeseTouch/lib/widgets/auth_form.dart
+++ CheeseTouch/lib/widgets/auth_form.dart
@@ -6,6 +6,19 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+  String _userEmail = '';
+  String _userName = '';
+  String _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState.validate();
+
+    if (isValid) {
+      _formKey.currentState.save();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -15,18 +28,43 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: <Widget>[
                   TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty || !value.contains('@'))
+                        return 'Please enter a valid email address.';
+                      return null;
+                    },
                     keyboardType: TextInputType.emailAddress,
                     decoration: InputDecoration(labelText: 'Email'),
+                    onSaved: (value) {
+                      _userEmail = value;
+                    },
                   ),
                   TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 4)
+                        return 'Enter atleast 4 characters';
+                      return null;
+                    },
                     decoration: InputDecoration(labelText: 'Username'),
+                    onSaved: (value) {
+                      _userName = value;
+                    },
                   ),
                   TextFormField(
+                    validator: (value) {
+                      if (value.isEmpty || value.length < 7)
+                        return 'Password must be 7 characters long.';
+                      return null;
+                    },
                     decoration: InputDecoration(labelText: 'Password'),
+                    onSaved: (value) {
+                      _userPassword = value;
+                    },
                     obscureText: true,
                   ),
                   SizedBox(
