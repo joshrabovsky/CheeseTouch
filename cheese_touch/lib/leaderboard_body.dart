24adbc20fb547495f3b287b1ce9eaad991d35afd
ghuang72 <gracejoanhuang@gmail.com>
2021-02-06 22:52:29 -0500

    added appbar and drawer

--- cheese_touch/lib/leaderboard_body.dart
+++ cheese_touch/lib/leaderboard_body.dart
@@ -1,10 +1,14 @@
 import "package:flutter/material.dart";
 
-import "./entries.dart";
+import "appbar.dart";
+import "drawer.dart";
 
-class LeaderboardBody extends StatelessWidget {
-  final double topHeight;
+class LeaderboardBody extends StatefulWidget {
+  @override
+  _LeaderboardBodyState createState() => _LeaderboardBodyState();
+}
 
+class _LeaderboardBodyState extends State<LeaderboardBody> {
   final String myUsername = "ghuang72";
 
   final TextStyle itemTextStyle = TextStyle(
@@ -12,8 +16,6 @@ class LeaderboardBody extends StatelessWidget {
     color: Colors.white,
     fontWeight: FontWeight.bold,
   );
-
-  LeaderboardBody(this.topHeight);
 
   final List<Map<String, Object>> entries = [
     {"rank": 1, "username": "hyuan", "points": 49002},
@@ -35,112 +37,126 @@ class LeaderboardBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          padding: EdgeInsets.only(
-              top: topHeight + 10, bottom: 10, left: 10, right: 10),
-          child: Text("LEADERBOARD",
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              )),
+    return Scaffold(
+      backgroundColor: Color(0xff231651),
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(70.0), child: MyAppBar()),
+      endDrawer: DrawerMenu(),
+      body: Container(
+        margin: EdgeInsets.all(0),
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/plainbackground.png"),
+              fit: BoxFit.fitWidth),
         ),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Text(
-                  "Rank",
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(10),
+              child: Text("LEADERBOARD",
                   style: TextStyle(
-                    fontSize: 16,
+                    fontSize: 30,
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Expanded(
-                flex: 6,
-                child: Container(
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    "Username",
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
+                  )),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      "Rank",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
-              ),
-              Expanded(
-                flex: 2,
-                child: Container(
-                  child: Text(
-                    "Points",
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
+                  Expanded(
+                    flex: 6,
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "Username",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
                     ),
                   ),
-                ),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                      child: Text(
+                        "Points",
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  padding: EdgeInsets.only(top: 0),
+                  itemCount: entries.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      color: entries[index]["username"] != myUsername
+                          ? Color.fromRGBO(88, 119, 146, 0.8)
+                          : Color.fromRGBO(244, 224, 77, 0.8),
+                      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                      elevation: 6,
+                      child: Container(
+                        height: 40,
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Container(
+                                width: 50,
+                                padding: EdgeInsets.all(10),
+                                child: Center(
+                                  child: Text("${entries[index]["rank"]}",
+                                      style: itemTextStyle),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 6,
+                                child: Container(
+                                  padding: EdgeInsets.all(10),
+                                  child: Text("${entries[index]["username"]}",
+                                      style: itemTextStyle),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 2,
+                                child: Container(
+                                  child: Text("${entries[index]["points"]}",
+                                      style: itemTextStyle),
+                                ),
+                              ),
+                            ]),
+                      ),
+                    );
+                  }),
+            ),
+          ],
         ),
-        Expanded(
-          child: ListView.builder(
-              padding: EdgeInsets.only(top: 0),
-              itemCount: entries.length,
-              itemBuilder: (BuildContext context, int index) {
-                return Card(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  color: entries[index]["username"] != myUsername
-                      ? Color.fromRGBO(88, 119, 146, 0.8)
-                      : Color.fromRGBO(244, 224, 77, 0.8),
-                  margin: EdgeInsets.symmetric(horizontal: 20, vertical: 8),
-                  elevation: 6,
-                  child: Container(
-                    height: 40,
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Container(
-                            width: 50,
-                            padding: EdgeInsets.all(10),
-                            child: Center(
-                              child: Text("${entries[index]["rank"]}",
-                                  style: itemTextStyle),
-                            ),
-                          ),
-                          Expanded(
-                            flex: 6,
-                            child: Container(
-                              padding: EdgeInsets.all(10),
-                              child: Text("${entries[index]["username"]}",
-                                  style: itemTextStyle),
-                            ),
-                          ),
-                          Expanded(
-                            flex: 2,
-                            child: Container(
-                              child: Text("${entries[index]["points"]}",
-                                  style: itemTextStyle),
-                            ),
-                          ),
-                        ]),
-                  ),
-                );
-              }),
-        ),
-      ],
+      ),
     );
   }
 }
--- cheese_touch/lib/main.dart
+++ cheese_touch/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import "./leaderboard_body.dart";
+import "./appbar.dart";
 
 void main() {
   runApp(MyApp());
@@ -26,49 +27,50 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final Color mainColor = Color(0xff231651);
 
-  final AppBar appBar = AppBar(
-    backgroundColor: Colors.transparent,
-    title: Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: <Widget>[
-        Expanded(
-          child: Container(
-            child: Image.asset(
-              "assets/images/appbarlogo.png",
-            ),
-          ),
-        ),
-      ],
-    ),
-  );
+  // final AppBar appBar = AppBar(
+  //   backgroundColor: Colors.transparent,
+  //   title: Row(
+  //     mainAxisAlignment: MainAxisAlignment.start,
+  //     crossAxisAlignment: CrossAxisAlignment.center,
+  //     children: <Widget>[
+  //       Expanded(
+  //         child: Container(
+  //           child: Image.asset(
+  //             "assets/images/appbarlogo.png",
+  //           ),
+  //         ),
+  //       ),
+  //     ],
+  //   ),
+  // );
 
   @override
   Widget build(BuildContext context) {
-    double totalHeight =
-        MediaQuery.of(context).padding.top + appBar.preferredSize.height;
+    // double totalHeight =
+    //     MediaQuery.of(context).padding.top + MyAppBar().preferredSize.height;
 
-    return Scaffold(
-      extendBodyBehindAppBar: true,
-      appBar: appBar,
-      endDrawer: Drawer(),
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        child: Stack(children: [
-          Container(color: mainColor),
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: AssetImage("assets/images/plainbackground.png"),
-                  fit: BoxFit.fitWidth),
-            ),
-          ),
-          Container(
-              child: Center(
-            child: LeaderboardBody(totalHeight),
-          ))
-        ]),
-      ),
-    );
+    return LeaderboardBody();
+    //Scaffold(
+    //   // extendBodyBehindAppBar: true,
+    //   appBar: MyAppBar(),
+    //   endDrawer: Drawer(),
+    //   body: Container(
+    //     width: MediaQuery.of(context).size.width,
+    //     child: Stack(children: [
+    //       Container(color: mainColor),
+    //       Container(
+    //         decoration: BoxDecoration(
+    //           image: DecorationImage(
+    //               image: AssetImage("assets/images/plainbackground.png"),
+    //               fit: BoxFit.fitWidth),
+    //         ),
+    //       ),
+    //       Container(
+    //           child: Center(
+    //         child: LeaderboardBody(totalHeight),
+    //       ))
+    //     ]),
+    //   ),
+    // );
   }
 }
